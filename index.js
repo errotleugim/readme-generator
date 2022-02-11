d1b42c25e69eb8dcf926c984b6f583482aceae19
errotleugim <94770602+errotleugim@users.noreply.github.com>
2022-02-11 11:30:23 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs')
+
+// TODO: Create an array of questions for user input
+const questions = [
+    "What is the project's title?", 
+    "Provide a short description of your project",
+    "Table of contents? (optional)",
+    "Installation instructions:",
+    "How does it work?",
+    "Credits"
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
