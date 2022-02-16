586e75bef5cae4de439c598b93d920dd811bc1a7
errotleugim <94770602+errotleugim@users.noreply.github.com>
2022-02-15 21:30:01 -0800

    minor adjustment for readability

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+const inquirer = require('inquirer');
+const fs = require('fs'); 
+const generate = require('./utils/generateMarkdown.js');
+const path = require('path');
+
 function init () {
 
-  const inquirer = require('inquirer');
-  const fs = require('fs'); 
-  const generate = require('./utils/generateMarkdown.js');
-  const path = require('path');
-  
+
   inquirer
     .prompt([
       { 
